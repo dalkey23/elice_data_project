52c48b5c4f7e26c8cac55962e0ef3adc5aa50a4e
[AI6/2반] 박재희 <dalkey23@gmail.com>
2023-03-16 17:53:59 +0900

    fix: cors

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,8 @@ const boardRouter = require("./boardRouter")
 const userRouter = require("./userRouter");
 
 const v1Router = express.Router();
-
+const cors = require('cors');
+v1Router.use(cors())
 v1Router.use("/posts", postRouter);
 v1Router.use("/board", boardRouter);
 v1Router.use("/users", userRouter);
